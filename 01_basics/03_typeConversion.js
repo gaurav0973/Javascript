ac600a09acf5fe46158b130fc64a86af08edcda0
gaurav0973 <151557489+gaurav0973@users.noreply.github.com>
2024-04-25 12:52:47 +0000

    Operations and concatination

--- 01_basics/03_typeConversion.js
+++ 01_basics/03_typeConversion.js
@@ -1,46 +1,85 @@
-let score = "gaurav"
-console.log(typeof score);
+// let score = "gaurav"
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber); // NaN => this is not a number 
-console.log(typeof NaN);
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); // NaN => this is not a number 
+// console.log(typeof NaN);
+
+// /*
+// converting into the Number **N in number will be capital
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1
+// "false" => 0
+// null => 0
+// undefined => NaN
+// "gaurav" => NaN
+// NOTE => type of NaN is number
+// */
+
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// /*
+// Converting into the boolean
+// 1 => true
+// 0 => false
+// "" => false
+// "gaurav" => true
+// */
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// /*
+// Converting into the string
+// 33 => "33"
+// */
+
+
+
+//**************OPERATIONS********************** */
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+console.log(2+2); //addition
+console.log(2-2); //subtraction
+console.log(2*2); //multiplication
+console.log(3**3); // 3 to the power 3
+console.log(2/2); //division
+console.log(2%2); //modulus -> remainder
+console.log("Now lets talk abourt some trickey things");
+
+let str1 = "hello"
+let str2 = " Gaurav"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+console.log(1 + 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 5 + "2" + 7 + 3);
+console.log(1 + 2 + "2");
 
 /*
-converting into the Number **N in number will be capital
-"33" => 33
-"33abc" => NaN
-"true" => 1
-"false" => 0
-null => 0
-undefined => NaN
-"gaurav" => NaN
-NOTE => type of NaN is number
+✍️✍️✍️Interview Question
+link => https://youtu.be/OeVE-8CIzys?si=HN_1dnG2JiGKKvhM
+if string is first then all are treaded as string
+if string is middle then  conversion -> till string -> string
+if string is last then first the conversion takes place then string 
 */
 
-
-let isLoggedIn = "";
-
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-
-
-/*
-Converting into the boolean
-1 => true
-0 => false
-"" => false
-"gaurav" => true
-*/
-
-
-let someNumber = 33
-let stringNumber = String(someNumber)
-
-console.log(stringNumber);
-console.log(typeof stringNumber);
-
-/*
-Converting into the string
-33 => "33"
-*/
