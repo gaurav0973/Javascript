3c454c0587c83e1dc440d40313358166f58cfee5
gaurav0973 <151557489+gaurav0973@users.noreply.github.com>
2024-04-25 12:09:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+//"use strict"; //->treat all JS code as newre version
+
+
+// alert(3+3) -->this does not works here because we are using nodejs, not the browser
+
+
+let name = "gaurav" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value -> khali hai
+// undefined => abhi value assign nahi hui hai
+// symbol => finding the uniqueness -> react
+// object 
+
+console.log("Gaurav");
+console.log(3+4);
+console.log(typeof 8);
+console.log(typeof "");
+console.log(typeof 4.6);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Symbol);
+
+
+
+/*
+   ✍️✍️✍️INTERVIEW
+   1. type of null is object
+   2. type of undefined is undefined
+   3. type of symbol is function 
+*/
+
+
